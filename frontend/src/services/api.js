e4fe55a40e81c6771b6612ec2f2bfe68f8923246
Atul <99653239+atulranjan9026@users.noreply.github.com>
2025-02-01 16:15:29 +0530

    Hello World!

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://atulranjan9026-project-managementexcel.onrender.com"; // Updated backend URL
 
 export const fetchProjects = async () => {
   const { data } = await axios.get(`${API_URL}/tasks`);
